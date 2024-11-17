970bf035792f009522960c3841f47f8206400c3b
Bruno <brunollemos@live.com>
2024-11-17 14:00:00 -0300

    adicionando e removendo classes de forma dinâmica

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const html = document.querySelector('html');
 
+const botoes = document.querySelectorAll('.app__card-button');
 const botaoIniciar = document.querySelector('.app__card-primary-button');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
@@ -25,15 +26,24 @@ function alterarContexto(contexto) {
   switch (contexto) {
     case 'foco':
       titulo.innerHTML = `Otimize sua produtividade,
-      <br><strong class="app__title-strong">mergulhe no que importa</strong>`;            
+      <br><strong class="app__title-strong">mergulhe no que importa</strong>`;
+      focoBt.classList.add('active');
+      curtoBt.classList.remove('active');
+      longoBt.classList.remove('active');
     break;
     case 'descanso-curto':
       titulo.innerHTML = `Que tal dar uma respirada?
       <br><strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+      curtoBt.classList.add('active');
+      focoBt.classList.remove('active');
+      longoBt.classList.remove('active');
     break;
     case 'descanso-longo':
       titulo.innerHTML = `Hora de voltar à superfície.
       <br><strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+      longoBt.classList.add('active');
+      focoBt.classList.remove('active');
+      curtoBt.classList.remove('active');
     break;
     default:
     break;
