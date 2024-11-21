0c7d33c1e1d038ce93bffd884a503dbe3a2d1c41
Bruno <brunollemos@live.com>
2024-11-21 15:40:00 -0300

    ajustando ícones no botão de começar/pausar

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const html = document.querySelector('html');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
-const banner = document.querySelector('.app__image');
-const titulo = document.querySelector('.app__title');
 const startPauseBt = document.querySelector('#start-pause');
-const musicaFocoInput = document.querySelector('#alternar-musica');
 const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const iniciarOuPausarBtIcone = document.querySelector('.app__card-primary-butto-icon');
 const tempoNaTela = document.querySelector('#timer');
+const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
 const audioPlay = new Audio('/sons/play.wav');
 const audioPausa = new Audio('/sons/pause.mp3');
@@ -94,11 +95,13 @@ function iniciarOuPausar() {
   audioPlay.play();
   intervaloId = setInterval(contagemRegressiva, 1000);  
   iniciarOuPausarBt.textContent = 'Pausar';
+  iniciarOuPausarBtIcone.setAttribute('src', `/imagens/pause.png`);
 }
 
 function zerar() {
   clearInterval(intervaloId);  
   iniciarOuPausarBt.textContent = 'Começar';
+  iniciarOuPausarBtIcone.setAttribute('src', `/imagens/play_arrow.png`);
   intervaloId = null; 
 }
 
